4691af7c0c743500dca04c35a1286be21bdd5fef
Fernanda Amaral <fernanda.amaral00@souunit.com.br>
2020-11-10 15:30:09 -0300

    Desempilhar elemento

--- src/com/ninha/estruturadados/pilha/Pilha.java
+++ src/com/ninha/estruturadados/pilha/Pilha.java
@@ -23,10 +23,15 @@ public class Pilha<T> extends EstruturaEstatica<T> {
 		return this.elementos[tamanho-1]; 
 	}
 
-
-	
-	
-	
-	
+	public T desempilha() { 
+		if(this.estaVazia()) {
+			return null; 
+		}
+		/* T elemento = this.elemento[tamanho - 1]; 
+		 * tamanho--; 
+		 * return elemento; */
+		
+		return this.elementos[--tamanho]; 
+	}
 	
 } 
--- src/com/ninha/estruturadados/pilha/teste/Teste.java
+++ src/com/ninha/estruturadados/pilha/teste/Teste.java
@@ -6,19 +6,18 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
+		//LIFO - last in first out - último a entrar, primeiro a sair
+		
 		Pilha<Integer> pilha = new Pilha<Integer>();
 		
-		System.out.println(pilha.topo());
-
-		System.out.println(pilha);
-		
-		pilha.empilha(1); 
+		pilha.empilha(1);
 		pilha.empilha(2);
+		pilha.empilha(3);
+		pilha.empilha(4);
 		
-		System.out.println(pilha.topo());
 		System.out.println(pilha);
-		
-		
+		System.out.println("Desempilhando elemento " + pilha.desempilha());
+		System.out.println(pilha);
 	}
 
 }
